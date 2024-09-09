0775459582002088e876350c504e17e863ef20f8
Abir Chodha <abirchodha1@gmail.com>
2024-09-09 05:43:12 +0530

    Disallow user to enter amount more than bill value

--- src/App.js
+++ src/App.js
@@ -177,7 +177,11 @@ function FormSplitBill({ selectedFriend }) {
       <input
         type="text"
         value={paidByUser}
-        onChange={(e) => setPaidByUser(Number(e.target.value))}
+        onChange={(e) =>
+          setPaidByUser(
+            Number(e.target.value) > bill ? paidByUser : Number(e.target.value)
+          )
+        }
       />
 
       <label>🧑‍🤝‍🧑 {selectedFriend.name}'s expense</label>
